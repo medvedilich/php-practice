00d92f95d5d3b4c69b5d0901a5403bcb027cd095
Ivan <87783500+medvedilich@users.noreply.github.com>
2022-02-04 00:29:55 +0300

    Update task_16_handler.php

--- task_16_handler.php
+++ task_16_handler.php
@@ -1,17 +1,6 @@
 
 <?php
-
-function get_type($file_name){
-    $index = strripos($file_name, '.');
-    return substr($file_name, $index + 1);
-}
-
-function addFile($array_name, $id, $path){
-    $file = $_FILES[$array_name];
-    $type = get_type($file['name']);
-    $file_name = $path.$id.'.'.$type;
-    return move_uploaded_file($file['tmp_name'], $file_name);
-}
+require 'Files.php';
 
 if(addFile('image', $_POST['id'], 'img\demo\gallery\\')){
     require('Database.php');
