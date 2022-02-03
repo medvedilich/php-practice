b9880bec8c0538f58507eabbabdda373246e6e2f
Ivan <87783500+medvedilich@users.noreply.github.com>
2022-02-04 00:31:49 +0300

    Update task_17_handler.php

--- task_17_handler.php
+++ task_17_handler.php
@@ -3,7 +3,6 @@
 unlink("img/demo/gallery/$_GET[id].$_GET[type]");
 
 require('Database.php');
-$db = new Database('localhost', 'root', '', 'practice');
-$db->deleteRows('images', ['id'=>$_GET['id']]);
+$db->deleteRows('images', ['id', $_GET['id'], '=']);
 header('location: task_17.php');
 exit;
