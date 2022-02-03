e51a7ed7ecc58c8f4fe64a98d2b08ec9d9626ad9
Ivan <87783500+medvedilich@users.noreply.github.com>
2022-02-04 00:05:18 +0300

    Update task_11_handler.php

--- task_11_handler.php
+++ task_11_handler.php
@@ -1,54 +1,14 @@
 <?php
-
-class Database{
-    private $pdo;
-    function __construct($hostname, $user, $password, $database){
-        $this->pdo = new PDO("mysql:host=$hostname;dbname=$database;", $user, $password);
-    }
-
-    function createRow($table, array $values){
-        $sql = "INSERT INTO $table(";
-        $fields = array_keys($values);
-        
-        foreach($fields as $field){
-            $sql .= $field.', ';
-        }
-        $sql = substr($sql, 0, strlen($sql)-2);
-        $sql .= ') VALUES (';
-
-        foreach($fields as $field){
-            $sql .= ':'.$field.', ';
-        }
-        $sql = substr($sql, 0, strlen($sql)-2);
-        $sql .= ')';
-
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute($values);
-    }
-
-    function getRows($table, ?array $requirements){
-        $sql = "SELECT * FROM $table ";
-        if(isset($requirements)){
-            $sql .= 'WHERE ';
-
-            $keys = array_keys($requirements);
-            foreach($keys as $key){
-                $sql .= $key.'=:'.$key.' AND ';
-            }
-            $sql = substr($sql, 0, strlen($sql)-5);
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute($requirements);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-    }
-}
+require 'Database.php';
 
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$match = $db->getRows(
+    'users', 
+    'email', 
+    ['email', $email, '=']);
 
-$db = new Database('localhost', 'root', '', 'practice');
-
-if($db->getRows('users', ['email'=>$email]) != null){
+if($match != null){
     session_start();
     $_SESSION['error'] = 'Этот эл адрес уже занят другим пользователем';
     header('location: task_11.php');
